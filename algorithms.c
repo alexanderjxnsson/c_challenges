d52da4ca9707797e8e1f96f6cc47c72e3e4708d0
Alexander Jönsson <aljon1@se.abb.com>
2022-10-26 12:42:48 +0200

    Forgot to remove math from algorithm c-file

--- algorithms.c
+++ algorithms.c
@@ -4,7 +4,7 @@
 void algorithm1();
 
 int main(){
-
+    algorithm1();
     return 0;
 }
 
@@ -22,20 +22,5 @@ void algorithm1(){
         printf("They are not the same:\n");
         a = a + b;
         printf("%d\n", a);
-    }
-}
-
-void reverse_number(){
-    int reverse = 0, rem;
-    int num[3] = {123, 208478933, -73634};
-
-    for (int i = 0; i < 3; i++){
-        while (num[i] != 0) {
-            rem = num[i] % 10;
-            reverse = reverse * 10 + rem;
-            num[i] /= 10;
-        }
-    printf("Reversed number is: %d\n", reverse);
-    reverse = 0;
     }
 }
